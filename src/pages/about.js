2d4f8c8db1c10c06fa8f57d830a9aafbb072b8fd
Kenneth Chen <efeichen@gmail.com>
2020-04-26 03:33:22 -0700

    Add content to pages

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+export default () => (
+  <Layout>
+    <section className="page-header">
+      <h1>About Us</h1>
+    </section>
+  </Layout>
+)
---
+++ src/pages/blog.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+export default () => (
+  <Layout>
+    <section className="page-header">
+      <h1>Blog</h1>
+    </section>
+  </Layout>
+)
