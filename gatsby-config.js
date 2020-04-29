539b204bb87ac02c4ec5ec541dba01e7d928f2a2
Kenneth Chen <efeichen@gmail.com>
2020-04-28 17:34:28 -0700

    Change Serif font

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
       resolve: "gatsby-plugin-web-font-loader",
       options: {
         google: {
-          families: ["PT Serif"],
+          families: ["Playfair Display"],
         },
         custom: {
           families: ["Inter"],
