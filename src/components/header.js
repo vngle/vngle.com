bdb3f0d3551d612672a2399a1f3ab89821ec1c1b
Kenneth Chen <efeichen@gmail.com>
2020-05-10 21:41:00 -0700

    Reorder navlinks

--- src/components/header.js
+++ src/components/header.js
@@ -2,35 +2,37 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import styled from "styled-components"
-import { Navbar, Nav, Button } from "react-bootstrap"
+import { Container, Navbar, Nav, Button } from "react-bootstrap"
 import logo from "../static/images/logo.png"
 
 const Header = ({ siteTitle }) => (
   <StyledHeader>
-    <Navbar collapseOnSelect expand="lg" className="p-5">
-      <Navbar.Brand>
-        <Link to="/" title={siteTitle}>
-          <img alt="Vngle logo" src={logo} height="47" />
-        </Link>
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse className="justify-content-end text-center">
-        <Nav>
-          <Button
-            href="https://docs.google.com/forms/d/1s6VKLzBLnfLDjUn5IWHwezZmQJZK-SBHjGGvTY27XSQ/viewform?edit_requested=true"
-            size="lg"
-          >
-            Join Us
-          </Button>
-          <Nav.Link>Where We Are Now</Nav.Link>
-          <Link to="/about" className="nav-link">
-            About
+    <Navbar collapseOnSelect expand="lg" className="p-4">
+      <Container>
+        <Navbar.Brand>
+          <Link to="/" title={siteTitle}>
+            <img alt="Vngle logo" src={logo} height="47" />
           </Link>
-          <Nav.Link href="https://docs.google.com/forms/d/1ymGkcekuWk_1SPl0r6lp9uleQKGbHE6dTx4aY1jrNT0/edit">
-            Tipline
-          </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse className="justify-content-end text-center">
+          <Nav>
+            <Nav.Link>Where We Are Now</Nav.Link>
+            <Nav.Link href="https://docs.google.com/forms/d/1ymGkcekuWk_1SPl0r6lp9uleQKGbHE6dTx4aY1jrNT0/edit">
+              Tipline
+            </Nav.Link>
+            <Link to="/about" className="nav-link">
+              About
+            </Link>
+            <Button
+              href="https://docs.google.com/forms/d/1s6VKLzBLnfLDjUn5IWHwezZmQJZK-SBHjGGvTY27XSQ/viewform?edit_requested=true"
+              size="lg"
+            >
+              Join Us!
+            </Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   </StyledHeader>
 )
