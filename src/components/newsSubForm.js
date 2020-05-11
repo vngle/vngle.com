a826443cf26494d4d8b2964175c4b919d1568454
Kenneth Chen <efeichen@gmail.com>
2020-05-10 21:41:30 -0700

    Improve hero section & subscribe form design

--- src/components/newsSubForm.js
+++ src/components/newsSubForm.js
@@ -1,55 +1,61 @@
 import React from "react"
-import { Form, Input, Button } from "antd"
-import {
-  MailOutlined,
-  EnvironmentOutlined,
-  LoadingOutlined,
-} from "@ant-design/icons"
-import "antd/dist/antd.css"
+import { Form, Button } from "react-bootstrap"
 
 export default ({ status, message, onSubmitted }) => {
   if (status !== "success") {
     return (
-      <Form
-        name="newsletter-subscribe"
-        initialValues={{ remember: true }}
-        onFinish={onSubmitted}
-      >
-        <Form.Item
-          name="EMAIL"
-          rules={[
-            { required: true, message: "Please enter your email address." },
-            {
-              type: "email",
-              message: "Please enter a valid email address.",
-            },
-          ]}
-        >
-          <Input
-            placeholder="Email"
-            prefix={<MailOutlined className="site-form-item-icon" />}
-            size="large"
-          />
-        </Form.Item>
-        <Form.Item name="city-zip">
-          <Input
-            placeholder="City/Zipcode"
-            prefix={<EnvironmentOutlined className="site-form-item-icon" />}
-            size="large"
-          />
-        </Form.Item>
-        <Form.Item>
-          <Button
-            type="primary"
-            size="large"
-            shape="round"
-            htmlType="submit"
-            loading={status === "sending"}
-          >
-            Subscribe
-          </Button>
-        </Form.Item>
+      <Form>
+        <Form.Group controlId="formMailchimp">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="text" placeholder="Enter city" />
+        </Form.Group>
+        <Button>Subscribe</Button>
       </Form>
+
+      // <Form
+      //   name="newsletter-subscribe"
+      //   initialValues={{ remember: true }}
+      //   onFinish={onSubmitted}
+      // >
+      //   <Form.Item
+      //     name="EMAIL"
+      //     rules={[
+      //       { required: true, message: "Please enter your email address." },
+      //       {
+      //         type: "email",
+      //         message: "Please enter a valid email address.",
+      //       },
+      //     ]}
+      //   >
+      //     <Input
+      //       placeholder="Email"
+      //       prefix={<MailOutlined className="site-form-item-icon" />}
+      //       size="large"
+      //     />
+      //   </Form.Item>
+      //   <Form.Item name="city-zip">
+      //     <Input
+      //       placeholder="City/Zipcode"
+      //       prefix={<EnvironmentOutlined className="site-form-item-icon" />}
+      //       size="large"
+      //     />
+      //   </Form.Item>
+      //   <Form.Item>
+      //     <Button
+      //       type="primary"
+      //       size="large"
+      //       shape="round"
+      //       htmlType="submit"
+      //       loading={status === "sending"}
+      //     >
+      //       Subscribe
+      //     </Button>
+      //   </Form.Item>
+      // </Form>
     )
   } else {
     return <p>{message}</p>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,94 +1,92 @@
-import React from "react"
-import { Button } from "antd"
-import { LoadingOutlined } from "@ant-design/icons"
-import "antd/dist/antd.css"
+import React, { useState, useEffect } from "react"
+import { Jumbotron, Container, Card, Row, Col } from "react-bootstrap"
+import styled from "styled-components"
 import Nanogram from "nanogram.js"
 import MailchimpSubscribe from "react-mailchimp-subscribe"
 
 import Layout from "../components/layout"
 import NewsSubForm from "../components/newsSubForm"
 import SEO from "../components/seo"
-import logo from "../../static/images/hero.svg"
-import MozillaLogo from "../../static/images/mozilla.png"
-import ColumbiaLogo from "../../static/images/columbia-tamer.png"
-import GoodieNationLogo from "../../static/images/goodie-nation.png"
+import HeroImg from "../static/images/hero.svg"
+import MozillaLogo from "../static/images/mozilla.png"
+import ColumbiaLogo from "../static/images/columbia-tamer.png"
+import GoodieNationLogo from "../static/images/goodie-nation.png"
 
-class IndexPage extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      instaFeed: null,
+const IndexPage = () => {
+  const [instaFeed, setInstaFeed] = useState(null)
+
+  useEffect(() => {
+    const fetchInstaFeed = async () => {
+      const instaParser = new Nanogram()
+      const response = await instaParser.getMediaByUsername("vnglestories")
+
+      setInstaFeed(response)
     }
-  }
 
-  instaParser = new Nanogram()
+    fetchInstaFeed()
+  }, [])
 
-  componentDidMount() {
-    this._asyncRequest = this.instaParser
-      .getMediaByUsername("vnglestories")
-      .then(instaFeed => {
-        this._asyncRequest = null
-        this.setState({ instaFeed })
-      })
-  }
+  if (instaFeed === null) {
+    return <div className="loading">LOADING</div>
+  } else {
+    return (
+      <Layout>
+        <SEO title="Decentralized grassroots news network for news deserts." />
 
-  componentWillUnmount() {
-    if (this._asyncRequest) {
-      this._asyncRequest.cancel()
-    }
-  }
+        <Jumbotron className="bg-primary rounded-0" as="section">
+          <HeroContainer>
+            <h1 className="text-center">
+              A decentralized grassroots news network for news deserts.
+            </h1>
+            <Row>
+              <Col className="text-center" lg md sm>
+                <img alt="Man reading newspaper" src={HeroImg}></img>
+              </Col>
+              <Col
+                lg
+                md
+                sm
+                className="d-flex flex-column justify-content-center"
+              >
+                <Card className="shadow border-0">
+                  <Card.Body>
+                    <Card.Title className="mb-4" as="h4">
+                      Sign up for our newsletter!
+                    </Card.Title>
+                    <MailchimpSubscribe
+                      url={process.env.GATSBY_MAILCHIMP_URL}
+                      render={({ subscribe, status, message }) => (
+                        <NewsSubForm
+                          status={status}
+                          message={message}
+                          onSubmitted={formData => subscribe(formData)}
+                        />
+                      )}
+                    />
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          </HeroContainer>
+        </Jumbotron>
 
-  render() {
-    if (this.state.instaFeed === null) {
-      return (
-        <div className="loading">
-          <LoadingOutlined />
-        </div>
-      )
-    } else {
-      return (
-        <Layout>
-          <SEO title="Decentralized grassroots news network for news deserts." />
-
-          <section className="page-header">
-            <div className="container">
-              <img alt="Logo" src={logo}></img>
-
-              <div className={"headline"}>
-                <h1>
-                  A decentralized grassroots news network for news deserts.
-                </h1>
-              </div>
-              <MailchimpSubscribe
-                url={process.env.GATSBY_MAILCHIMP_URL}
-                render={({ subscribe, status, message }) => (
-                  <NewsSubForm
-                    status={status}
-                    message={message}
-                    onSubmitted={formData => subscribe(formData)}
-                  />
-                )}
-              />
+        <section className="call-to-action">
+          <div className="flex-container">
+            <div className="content">
+              <h1>Think something needs coverage?</h1>
+              <p>
+                Request special coverage on your city or upload your own content
+                to show what's happening locally.
+              </p>
             </div>
-          </section>
-
-          <section className="call-to-action">
-            <div className="flex-container">
-              <div className="content">
-                <h1>Think something needs coverage?</h1>
-                <p>
-                  Request special coverage on your city or upload your own
-                  content to show what's happening locally.
-                </p>
-              </div>
-              <div className="coverage-action">
-                <Button size="large" shape="round">
+            <div className="coverage-action">
+              {/* <Button size="large" shape="round">
                   <a href="https://docs.google.com/forms/d/e/1FAIpQLSdn0-K37ukwXDy-A5KjNrvuGKAsGgbO-Aqk6i0uVllwf5SNNg/viewform">
                     Request Coverage
                   </a>
-                </Button>
+                </Button> */}
 
-                {/* <h2>OR</h2>
+              {/* <h2>OR</h2>
                   <Form.Item
                     name="dragger"
                     valuePropName="fileList"
@@ -106,52 +104,66 @@ class IndexPage extends React.Component {
                       </p>
                     </Upload.Dragger>
                   </Form.Item> */}
-              </div>
             </div>
-          </section>
+          </div>
+        </section>
 
-          <section className="stories container">
-            <div className="header">
-              <img
-                alt="Instagram post"
-                src={this.state.instaFeed.profile.profile_pic_url}
-                className="avatar"
-              />
-              <h2>@vnglestories</h2>
-            </div>
-            <div className="gallery">
-              {this.state.instaFeed.profile.edge_owner_to_timeline_media.edges.map(
-                (post, i) => (
-                  <div className="post" key={i}>
-                    <a
-                      href={`https://www.instagram.com/p/${post.node.shortcode}`}
-                    >
-                      <img alt="post" src={post.node.thumbnail_src} />
-                    </a>
-                  </div>
-                )
-              )}
-            </div>
-          </section>
+        <section className="stories container">
+          <div className="header">
+            <img
+              alt="Instagram post"
+              src={instaFeed.profile.profile_pic_url}
+              className="avatar"
+            />
+            <h2>@vnglestories</h2>
+          </div>
+          <div className="gallery">
+            {instaFeed.profile.edge_owner_to_timeline_media.edges.map(
+              (post, i) => (
+                <div className="post" key={i}>
+                  <a
+                    href={`https://www.instagram.com/p/${post.node.shortcode}`}
+                  >
+                    <img alt="post" src={post.node.thumbnail_src} />
+                  </a>
+                </div>
+              )
+            )}
+          </div>
+        </section>
 
-          <section className="worked-with">
-            <h1>Who We've Worked With</h1>
-            <div className="flex-container">
-              <a href="https://www.mozilla.org">
-                <img alt="Mozilla logo" src={MozillaLogo} />
-              </a>
-              <a href="https://www8.gsb.columbia.edu/socialenterprise/">
-                <img alt="Columbia Tamer Center logo" src={ColumbiaLogo} />
-              </a>
-              <a href="https://goodienation.org/">
-                <img alt="Goodie Nation logo" src={GoodieNationLogo} />
-              </a>
-            </div>
-          </section>
-        </Layout>
-      )
-    }
+        <section className="worked-with">
+          <h1>Who We've Worked With</h1>
+          <div className="flex-container">
+            <a href="https://www.mozilla.org">
+              <img alt="Mozilla logo" src={MozillaLogo} />
+            </a>
+            <a href="https://www8.gsb.columbia.edu/socialenterprise/">
+              <img alt="Columbia Tamer Center logo" src={ColumbiaLogo} />
+            </a>
+            <a href="https://goodienation.org/">
+              <img alt="Goodie Nation logo" src={GoodieNationLogo} />
+            </a>
+          </div>
+        </section>
+      </Layout>
+    )
   }
 }
+
+const HeroContainer = styled(Container)`
+  & {
+    h1 {
+      font-size: calc(9px + 4vw);
+      font-weight: 900;
+      margin-top: 0.7em;
+      margin-bottom: 1.5em;
+    }
+
+    img {
+      width: calc(100px + 25vw);
+    }
+  }
+`
 
 export default IndexPage
