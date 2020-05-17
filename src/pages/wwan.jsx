1255504fc61f713c3e1e45416f9c12aecc4330ad
Kenneth Chen <efeichen@gmail.com>
2020-05-17 00:41:51 -0600

    Init wwan

--- src/pages/wwan.jsx
+++ src/pages/wwan.jsx
@@ -1,11 +1,18 @@
 import React from "react"
+import { Container, Jumbotron } from "react-bootstrap"
 
 import Layout from "../components/layout"
 
 export default () => (
   <Layout>
-    <section className="page-header">
-      <h1>Where We Are Now</h1>
-    </section>
+    <Jumbotron className="bg-primary rounded-0" as="section">
+      <Container>
+        <h1>Where We Are Now</h1>
+      </Container>
+    </Jumbotron>
+
+    <Container as="article">
+      <h2>Under Construction...</h2>
+    </Container>
   </Layout>
 )
