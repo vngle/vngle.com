dabdb257b4e4d286c9909b2febe3644979b4b864
Kenneth Chen <efeichen@gmail.com>
2020-05-21 10:33:21 -0700

    Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,12 @@ module.exports = {
     },
     `gatsby-plugin-sass`,
     `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-96150653-1",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
