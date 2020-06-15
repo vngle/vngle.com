f1ed428b347c92335f45368ccac49af627cad007
Kenneth Chen <efeichen@gmail.com>
2020-06-15 14:45:17 -0700

    Fix slider video issue

--- src/components/landing/storySlider.jsx
+++ src/components/landing/storySlider.jsx
@@ -73,6 +73,7 @@ const StorySlider = () => {
                       autoPlay
                       muted
                       caption={caption}
+                      playsInline
                     />
                   ) : (
                     <img
