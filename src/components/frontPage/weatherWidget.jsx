d7e5242ce43440295bd2670cfd3fb100b27b7710
Kenneth Chen <efeichen@gmail.com>
2020-06-18 21:47:37 -0700

    Add date, weather, and bookmark components

---
+++ src/components/frontPage/weatherWidget.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+import PropTypes from "prop-types"
+
+const WeatherWidget = ({ cityId }) => {
+  const [weatherData, setWeatherData] = useState(null)
+
+  const fetchWeather = async reqConfig => {
+    const resp = await axios(reqConfig)
+
+    console.log(resp.data)
+    setWeatherData(resp.data)
+  }
+
+  useEffect(() => {
+    const endpoint = "https://api.openweathermap.org/data/2.5/weather"
+
+    fetchWeather({
+      method: "get",
+      url: endpoint,
+      params: {
+        id: cityId,
+        units: "imperial",
+        appid: process.env.GATSBY_WEATHER_API_KEY,
+      },
+    })
+  }, [cityId])
+
+  return (
+    weatherData !== null && (
+      <div>
+        <img
+          src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
+          className="d-inline"
+        ></img>
+        <b>{weatherData.main.temp}&deg;F</b>{" "}
+        {weatherData.weather[0].description}
+      </div>
+    )
+  )
+}
+
+WeatherWidget.propTypes = {
+  cityId: PropTypes.number.isRequired,
+}
+
+export default WeatherWidget
--- src/pages/collegepark.jsx
+++ src/pages/collegepark.jsx
@@ -1,20 +1,40 @@
 import React from "react"
 import styled from "styled-components"
 import { Container, Jumbotron, Row, Col } from "react-bootstrap"
+import { FcBookmark } from "react-icons/fc"
+import moment from "moment"
 
-import SEO from "../components/seo"
 import Layout from "../components/layout"
+import SEO from "../components/seo"
 import Image from "../components/image"
 import InstaFeed from "../components/frontPage/instaFeed"
 import NewsFeed from "../components/frontPage/newsFeed"
+import WeatherWidget from "../components/frontPage/weatherWidget"
 
 export default () => (
   <Layout>
     <SEO title="College Park Home" />
-    <StyledJumbotron className="position-relative">
+    <StyledJumbotron>
       <Image src="collegepark/cp.jpg" alt="Atlanta city view" bg />
-      <h1 className="display-1 font-weight-bold">College Park</h1>
+      <Row className="align-items-end">
+        <Col>
+          <h1 className="display-1">College Park</h1>
+          <Row as={Col}>
+            {moment().format("dddd, MMMM D, YYYY")}
+            <WeatherWidget cityId={4188815} />
+          </Row>
+        </Col>
+        <Col md="auto">
+          <h4>
+            <FcBookmark />
+            Bookmark Me!
+          </h4>
+        </Col>
+      </Row>
     </StyledJumbotron>
+
+    <Container fluid></Container>
+
     <ContentContainer fluid>
       <Row>
         <Col lg={8} md={7} xs={12}>
@@ -37,14 +57,29 @@ export default () => (
 )
 
 const StyledJumbotron = styled(Jumbotron)`
+  position: relative;
   background-color: transparent;
   color: ${props => props.theme.colors.light};
   background: linear-gradient(
     180deg,
     rgba(33, 37, 41, 0) 0%,
     rgba(33, 37, 41, 0.3) 50%,
-    rgba(33, 37, 41, 0.7) 100%
+    rgba(33, 37, 41, 0.9) 100%
   );
+  padding-top: 9rem;
+  padding-bottom: 2rem;
+
+  h1 {
+    font-weight: 800;
+  }
+
+  h4 {
+    font-family: Inter, sans-serif;
+  }
+
+  .row {
+    align-items: center;
+  }
 `
 
 const ContentContainer = styled(Container)`
