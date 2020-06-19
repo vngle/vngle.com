c0fede35b8d5be3a111486e31b9d137ce860c3f2
Kenneth Chen <efeichen@gmail.com>
2020-06-18 21:48:09 -0700

    Remove email field (will fill out in separate page)

--- src/components/landing/simpleSubForm.jsx
+++ src/components/landing/simpleSubForm.jsx
@@ -5,14 +5,7 @@ const SimpleSubForm = () => {
   return (
     <Form action={process.env.GATSBY_MAILCHIMP_URL} method="post">
       <InputGroup className="mb-3">
-        <Form.Control
-          placeholder="Enter email for updates"
-          aria-label="Enter email for updates"
-          aria-describedby="mailchimp-email-form"
-          type="email"
-          name="EMAIL"
-        />
-        <Form.Control type="text" placeholder="Enter zip code" name="ZIPCODE" />
+        <Form.Control type="text" placeholder="Your zip code" name="ZIPCODE" />
         <InputGroup.Append>
           <Button type="submit">Sign Up</Button>
         </InputGroup.Append>
