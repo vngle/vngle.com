c97309ae3cc96f6220a33afb58963d39a9f5741c
Kenneth Chen <efeichen@gmail.com>
2020-08-08 17:11:14 -0700

    Make Mailchimp form opens in new tab

--- src/components/landing/simpleSubForm.jsx
+++ src/components/landing/simpleSubForm.jsx
@@ -3,7 +3,12 @@ import { Form, InputGroup, Button } from "react-bootstrap"
 
 const SimpleSubForm = () => {
   return (
-    <Form action={process.env.GATSBY_MAILCHIMP_URL} method="post">
+    <Form
+      action={process.env.GATSBY_MAILCHIMP_URL}
+      method="post"
+      target="_blank"
+      rel="noreferrer"
+    >
       <InputGroup className="mb-3">
         <Form.Control type="text" placeholder="Your zip code" name="ZIPCODE" />
         <InputGroup.Append>
