9af6ceb520a44c8fbcad1398cedcaecc00c3f3f5
Kenneth Chen <efeichen@gmail.com>
2020-08-16 14:24:46 -0700

    Fix careers page redirect

---
+++ src/pages/careers.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const CareersPage = () => {
+  return (
+    <>
+      In Development... Check out our{" "}
+      <a href="https://www.notion.so/vngle/Careers-Vngle-ef8a5667e86b4c129a88538d04ae3f00">
+        Notion careers page
+      </a>
+      for now!
+    </>
+  )
+}
+
+export default CareersPage
