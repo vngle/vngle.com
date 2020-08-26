b319b2932e03656473c6b929364ab182590015cd
Kenneth Chen <efeichen@gmail.com>
2020-08-25 20:11:10 -0700

    Add doc & external prop

--- src/components/link.jsx
+++ src/components/link.jsx
@@ -1,17 +1,35 @@
 import React from "react"
 import { Link as GatsbyLink } from "gatsby"
 
-// Since DOM elements <a> cannot receive activeClassName
-// and partiallyActive, destructure the prop here and
-// pass it only to GatsbyLink
-const Link = ({ children, to, activeClassName, partiallyActive, ...other }) => {
+/**
+ * Wrapper around gatsby-link to return normal anchor element when "to" attribute is an external link
+ *
+ * Since DOM elements <a> cannot receive activeClassName
+ * and partiallyActive, destructure the prop here and
+ * pass it only to GatsbyLink
+ *
+ * @param {JSX} children - children of link elements (Gatsby Link or <a>)
+ * @param {String} to - route to this link
+ * @param {String} activeClassName - class name that will only be added to the Link when the current item is active
+ * @param {Boolean} partiallyActive - set whether active style should be shown for partially matched links
+ * @param {Boolean} external - explicitly use anchor element
+ * @param {Object} other - other attributes passed to gatsby-link
+ */
+const Link = ({
+  children,
+  to,
+  activeClassName,
+  partiallyActive,
+  external,
+  ...other
+}) => {
   // Tailor the following test to your environment.
   // This example assumes that any internal link (intended for Gatsby)
   // will start with exactly one slash, and that anything else is external.
-  const internal = /^\/(?!\/)/.test(to)
+  const isGatsbyLink = /^\/(?!\/)/.test(to) && !external
 
   // Use Gatsby Link for internal links, and <a> for others
-  if (internal) {
+  if (isGatsbyLink) {
     return (
       <GatsbyLink
         to={to}
