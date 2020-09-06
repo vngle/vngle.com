56ae4455d372e5f45224124e30136b86638ac4ce
Kenneth Chen <efeichen@gmail.com>
2020-09-05 21:30:42 -0700

    Add fallback news thumbnail when no thumbnails given

--- src/components/frontPage/newsFeed.jsx
+++ src/components/frontPage/newsFeed.jsx
@@ -1,16 +1,17 @@
-import React, { useState, useEffect } from "react"
-import { Row, Col, Spinner, Image } from "react-bootstrap"
-import styled from "styled-components"
-import axios from "axios"
+import React, { useState, useEffect } from "react";
+import { Row, Col, Spinner, Image } from "react-bootstrap";
+import { FaNewspaper } from "react-icons/fa";
+import styled from "styled-components";
+import axios from "axios";
 
 const NewsFeed = () => {
-  const [newsFeed, setNewsFeed] = useState(null)
+  const [newsFeed, setNewsFeed] = useState(null);
 
   const fetchNews = async reqConfig => {
-    const resp = await axios(reqConfig)
+    const resp = await axios(reqConfig);
 
-    setNewsFeed(resp.data.value)
-  }
+    setNewsFeed(resp.data.value);
+  };
 
   useEffect(() => {
     fetchNews({
@@ -22,8 +23,8 @@ const NewsFeed = () => {
       params: {
         q: "CollegePark AND georgia",
       },
-    })
-  }, [])
+    });
+  }, []);
 
   return newsFeed === null ? (
     <LoadingWrapper className="d-flex flex-column justify-content-center">
@@ -34,22 +35,35 @@ const NewsFeed = () => {
   ) : (
     <div>
       {newsFeed.map((article, i) => {
-        const thumbnail = article.image
-          ? article.image.thumbnail.contentUrl
-          : article.provider[0].image.thumbnail.contentUrl
+        console.log(article);
+
+        let Thumbnail;
+
+        // fallback logic for article card image
+        if (article.image || article.provider[0].image) {
+          Thumbnail = () => (
+            <Image
+              rounded
+              src={
+                article.image
+                  ? article.image.thumbnail.contentUrl
+                  : article.provider[0].image.thumbnail.contentUrl
+              }
+              alt="news thumbnail"
+              width="100%"
+            />
+          );
+        } else {
+          Thumbnail = () => <NewsIcon />;
+        }
 
         return (
           <Row
             key={i}
             className="bg-white mb-4 mx-0 p-md-3 p-4 align-items-center rounded shadow-sm position-relative"
           >
-            <Col md={4} sm={2} xs={12} className="mb-4 mb-sm-0">
-              <Image
-                rounded
-                src={thumbnail}
-                alt="news thumbnail"
-                width="100%"
-              />
+            <Col md={4} sm={2} xs={12} className="mb-4 mb-sm-0 text-center">
+              <Thumbnail />
             </Col>
             <Col className="position-static">
               <p>
@@ -65,11 +79,11 @@ const NewsFeed = () => {
               <p className="mb-0 text-muted">{article.provider[0].name}</p>
             </Col>
           </Row>
-        )
+        );
       })}
     </div>
-  )
-}
+  );
+};
 
 const LoadingWrapper = styled.div`
   height: 50vh;
@@ -78,6 +92,11 @@ const LoadingWrapper = styled.div`
     width: 100px;
     height: 100px;
   }
-`
+`;
 
-export default NewsFeed
+const NewsIcon = styled(FaNewspaper)`
+  font-size: 50px;
+  color: ${props => props.theme.colors.dark};
+`;
+
+export default NewsFeed;
