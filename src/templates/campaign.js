49aa42b18eec064d249c01fd3678a94460b3841b
Kenneth Chen <efeichen@gmail.com>
2020-09-06 00:13:10 -0700

    Use axios instead

--- src/templates/campaign.js
+++ src/templates/campaign.js
@@ -4,6 +4,8 @@ import { Container, Row, Col, Jumbotron } from "react-bootstrap";
 import styled from "styled-components";
 import VideoThumbnail from "react-video-thumbnail";
 
+import axios from "axios";
+
 import SEO from "../components/Seo";
 import Layout from "../components/Layout";
 
@@ -12,7 +14,7 @@ export default ({ pageContext: { campaign } }) => {
     if (file.contentType.startsWith("image")) {
       return <img alt="story" src={fixed.src} className="shadow" />;
     } else if (file.contentType.startsWith("video")) {
-      fetch(file.url).then(resp => console.log(resp));
+      axios(file.url).then(resp => console.log(resp));
       return (
         <VideoThumbnailContainer>
           <VideoThumbnail videoUrl={file.url} />
