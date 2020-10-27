22de498ffe175c784af581a2f49764cad9030287
Kenneth Chen <efeichen@gmail.com>
2020-10-26 21:32:58 -0700

    Add Marcus to about team section

--- static/images/about/profiles.js
+++ static/images/about/profiles.js
@@ -3,5 +3,6 @@ import Jaime from "./jaime.jpg";
 import Kenneth from "./kenneth.jpg";
 import Matthew from "./matthew.jpg";
 import Majestic from "./majestic.jpg";
+import Marcus from "./marcus.jpg";
 
-export default [Blake, Jaime, Matthew, Kenneth, Majestic];
+export default [Blake, Jaime, Matthew, Kenneth, Marcus, Majestic];
