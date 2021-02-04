aee7e7eb3f2b74bdb80403b39b9f4445faf11d9e
Kenneth Chen <efeichen@gmail.com>
2021-02-04 01:16:33 -0800

    Add Ampliy video info

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,84 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateVodAsset = /* GraphQL */ `
+  subscription OnCreateVodAsset {
+    onCreateVodAsset {
+      id
+      title
+      caption
+      author
+      tags
+      video {
+        id
+        createdAt
+        updatedAt
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateVodAsset = /* GraphQL */ `
+  subscription OnUpdateVodAsset {
+    onUpdateVodAsset {
+      id
+      title
+      caption
+      author
+      tags
+      video {
+        id
+        createdAt
+        updatedAt
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteVodAsset = /* GraphQL */ `
+  subscription OnDeleteVodAsset {
+    onDeleteVodAsset {
+      id
+      title
+      caption
+      author
+      tags
+      video {
+        id
+        createdAt
+        updatedAt
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onCreateVideoObject = /* GraphQL */ `
+  subscription OnCreateVideoObject {
+    onCreateVideoObject {
+      id
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateVideoObject = /* GraphQL */ `
+  subscription OnUpdateVideoObject {
+    onUpdateVideoObject {
+      id
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteVideoObject = /* GraphQL */ `
+  subscription OnDeleteVideoObject {
+    onDeleteVideoObject {
+      id
+      createdAt
+      updatedAt
+    }
+  }
+`;
