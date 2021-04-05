35be36fc14dc8ff50e843d439380f03651e15909
Kenneth Chen <efeichen@gmail.com>
2021-04-04 18:25:23 -0700

    Add Plerdy

---
+++ src/html.js
@@ -0,0 +1,47 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default function HTML(props) {
+  return (
+    <html {...props.htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        {props.headComponents}
+      </head>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+        <div
+          key={`body`}
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
+        {props.postBodyComponents}
+
+        <script type="text/javascript" defer>
+          var _protocol = (("https:" == document.location.protocol) ? "
+          https://" : " http://"); var _site_hash_code =
+          "f318aadce9a499db26f3b6cff9a60606"; var _suid = 15623;
+        </script>
+        <script
+          type="text/javascript"
+          defer
+          src="https://d.plerdy.com/public/js/click/main.js"
+        ></script>
+      </body>
+    </html>
+  );
+}
+
+HTML.propTypes = {
+  htmlAttributes: PropTypes.object,
+  headComponents: PropTypes.array,
+  bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.array,
+  body: PropTypes.string,
+  postBodyComponents: PropTypes.array,
+};
