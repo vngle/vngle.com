82d9eab0f0149dc426b893c916ee4b659a5b41a1
Kenneth Chen <efeichen@gmail.com>
2021-07-20 18:18:36 -0700

    make vngleforcities page responsive

--- src/views/vngleforcities/MainPage.js
+++ src/views/vngleforcities/MainPage.js
@@ -8,15 +8,17 @@ import { StaticImage } from "gatsby-plugin-image";
 const MainPage = () => {
   return (
     <MainPageContainer>
-      <SplitSection background="var(--bs-primary)">
-        <Col className="content" md={6}>
-          <p className="sup">Vngle for Cities</p>
-          <Heading>
-            <u>Your city + our grassroots operations</u>: let’s create deeper
-            connection and representation for your area.
-          </Heading>
+      <SplitSection background="var(--bs-primary)" className="gy-3">
+        <Col md={6} xs={12}>
+          <div className="content p-md-5">
+            <p className="sup">Vngle for Cities</p>
+            <Heading>
+              <u>Your city + our grassroots operations</u>: let’s create deeper
+              connection and representation for your area.
+            </Heading>
+          </div>
         </Col>
-        <Col className="image" md={6}>
+        <Col className="image" md={6} xs={12}>
           <StaticImage
             src="https://source.unsplash.com/XV81dRUMzpo"
             alt="City of Atlanta, GA"
@@ -24,7 +26,7 @@ const MainPage = () => {
         </Col>
       </SplitSection>
 
-      <Section width="90%">
+      <Section width="90%" className="p-md-4">
         <Heading weight="500" lineHeight="1.3em">
           Substantive local impact comes from grassroots up approaches, not top
           down thinking.{" "}
@@ -39,7 +41,7 @@ const MainPage = () => {
         </Heading>
       </Section>
 
-      <Section background="#1ee592">
+      <Section background="#1ee592" className="p-md-5">
         <FcConferenceCall />
         <Heading>Interested in becoming a local partner?</Heading>
         <Button
@@ -55,29 +57,32 @@ const MainPage = () => {
         <Heading center>Our local services</Heading>
       </Section>
 
-      <SplitSection background="#3cf">
-        <Col className="content" md={6}>
-          <p className="sup">COVERAGE</p>
-          <Heading>
-            Get organic grassroots video content curated for the local
-            communities you target.
-          </Heading>
-          <p>
-            Our coverage helps organizations stay connected and build their
-            presence in the cities they care about. We can work with your media
-            team or you can outsource your media needs to us and we’ll create an
-            engaging grassroots content strategy to help you stay connected to,
-            and have a presence among the people you seek to serve.
-          </p>
-          <Button
-            size="lg"
-            variant="light"
-            href="https://forms.gle/e6y4Gkna5S9kymdE8"
-          >
-            Let's cover you
-          </Button>
+      <SplitSection background="#3cf" className="gy-3">
+        <Col md={6} xs={12}>
+          <div className="content p-md-5">
+            <p className="sup">COVERAGE</p>
+            <Heading>
+              Get organic grassroots video content curated for the local
+              communities you target.
+            </Heading>
+            <p>
+              Our coverage helps organizations stay connected and build their
+              presence in the cities they care about. We can work with your
+              media team or you can outsource your media needs to us and we’ll
+              create an engaging grassroots content strategy to help you stay
+              connected to, and have a presence among the people you seek to
+              serve.
+            </p>
+            <Button
+              size="lg"
+              variant="light"
+              href="https://forms.gle/e6y4Gkna5S9kymdE8"
+            >
+              Let's cover you
+            </Button>
+          </div>
         </Col>
-        <Col className="image" md={6}>
+        <Col className="image" md={6} xs={12}>
           <StaticImage
             src="https://source.unsplash.com/SRReffF4vFA"
             alt="Journalist with a camera"
@@ -85,26 +90,29 @@ const MainPage = () => {
         </Col>
       </SplitSection>
 
-      <SplitSection background="#ff52db">
-        <Col className="content" md={6} xs={{ order: "last" }}>
-          <p className="sup">ENGAGE</p>
-          <Heading>
-            Get a wave of on-demand grassroots direct community engagement
-            support.
-          </Heading>
-          <p>
-            Through Engage, Vngle keeps your finger on the pulse of the shifting
-            environment around your local area. Think of us as your localized
-            census service: we craft and deploy strategic field plans to reach
-            local areas directly to gain or relay updated information.
-          </p>
-          <Button
-            size="lg"
-            variant="light"
-            href="https://forms.gle/e6y4Gkna5S9kymdE8"
-          >
-            Let's engage your city
-          </Button>
+      <SplitSection background="#ff52db" className="gy-3">
+        <Col md={6} md={{ order: "last" }}>
+          <div className="content p-md-5">
+            <p className="sup">ENGAGE</p>
+            <Heading>
+              Get a wave of on-demand grassroots direct community engagement
+              support.
+            </Heading>
+            <p>
+              Through Engage, Vngle keeps your finger on the pulse of the
+              shifting environment around your local area. Think of us as your
+              localized census service: we craft and deploy strategic field
+              plans to reach local areas directly to gain or relay updated
+              information.
+            </p>
+            <Button
+              size="lg"
+              variant="light"
+              href="https://forms.gle/e6y4Gkna5S9kymdE8"
+            >
+              Let's engage your city
+            </Button>
+          </div>
         </Col>
         <Col className="image" md={6}>
           <StaticImage
@@ -114,28 +122,31 @@ const MainPage = () => {
         </Col>
       </SplitSection>
 
-      <SplitSection background="var(--bs-primary)">
-        <Col className="content" md={6}>
-          <p className="sup">INSIGHTS</p>
-          <Heading>
-            Get research and consultation on how to address unique concerns that
-            matter most to your city.
-          </Heading>
-          <p>
-            With Vngle Insights, we listen carefully to your city to ensure
-            everyone’s voices are heard, to get you the data needed to make the
-            best decisions. Our insights make you aware of the matters that are
-            important to constituents. We offer you our grassroots knowledge
-            from working across 21+ cities to give you an advantage in devising
-            positive strategies to address your current challenges.
-          </p>
-          <Button
-            size="lg"
-            variant="light"
-            href="https://forms.gle/e6y4Gkna5S9kymdE8"
-          >
-            Let's work
-          </Button>
+      <SplitSection background="var(--bs-primary)" className="gy-3">
+        <Col md={6}>
+          <div className="content p-md-5">
+            <p className="sup">INSIGHTS</p>
+            <Heading>
+              Get research and consultation on how to address unique concerns
+              that matter most to your city.
+            </Heading>
+            <p>
+              With Vngle Insights, we listen carefully to your city to ensure
+              everyone’s voices are heard, to get you the data needed to make
+              the best decisions. Our insights make you aware of the matters
+              that are important to constituents. We offer you our grassroots
+              knowledge from working across 21+ cities to give you an advantage
+              in devising positive strategies to address your current
+              challenges.
+            </p>
+            <Button
+              size="lg"
+              variant="light"
+              href="https://forms.gle/e6y4Gkna5S9kymdE8"
+            >
+              Let's work
+            </Button>
+          </div>
         </Col>
         <Col className="image" md={6}>
           <StaticImage
@@ -170,7 +181,7 @@ const MainPage = () => {
 const Heading = styled.h1`
   font-family: "Inter", sans-serif;
   font-weight: ${(props) => props.weight || "bold"};
-  font-size: ${(props) => props.size || "2.6rem"};
+  font-size: clamp(1rem, 7vw, ${(props) => props.size || "2.6rem"});
   text-align: ${(props) => (props.center ? "center" : "left")};
   line-height: ${(props) => props.lineHeight};
   color: ${(props) => props.color};
@@ -187,12 +198,13 @@ const SplitSection = styled(Row)`
 
   .content {
     background: ${(props) => props.background};
-    padding: 5rem;
+    padding: 2rem;
     border-radius: 10px;
+    height: 100%;
 
     p.sup {
       font-weight: bold;
-      font-size: 1.5rem;
+      font-size: clamp(1rem, 2.5vw, 1.5rem);
     }
   }
 
@@ -209,7 +221,7 @@ const SplitSection = styled(Row)`
 const Section = styled.section`
   margin-top: 2rem;
   margin-bottom: 2rem;
-  padding: 4rem;
+  padding: 1rem;
   width: ${(props) => props.width};
   background: ${(props) => props.background};
   border-radius: 10px;
