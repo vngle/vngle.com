22db07270c40f6b1cda5a5e0c74151f12113d742
Kenneth Chen <efeichen@gmail.com>
2021-09-01 15:32:53 -0700

    Revert "edit team roster"
    This reverts commit 2c428d5b251bee3c4ceeafff45d194b7bca09e1b.

--- static/images/about/profiles.js
+++ static/images/about/profiles.js
@@ -3,8 +3,9 @@ import Jaime from "./jaime.jpg";
 import Kenneth from "./kenneth.jpg";
 import Majestic from "./majestic.png";
 import Marcus from "./marcus.jpg";
+import Carley from "./carley.jpg";
 import Don from "./don.jpeg";
 
-const profiles = [Blake, Jaime, Majestic, Marcus, Kenneth, Don];
+const profiles = [Blake, Carley, Jaime, Majestic, Marcus, Kenneth, Don];
 
 export default profiles;
