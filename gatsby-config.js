d4c5e729eda93ec4278e056c4e33441ea4223681
Chelsea Alcinord <59550990+seasea19@users.noreply.github.com>
2022-07-12 19:20:08 -0400

    modified

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,6 +55,30 @@ module.exports = {
         shortname: `vngle`,
       },
     },
+    
+    {
+      resolve: `gatsby-plugin-gdpr-cookies`,
+      options: {
+        googleAnalytics: {
+          trackingId: 'UA-96150653-1', // leave empty if you want to disable the tracker
+          cookieName: 'gatsby-gdpr-google-analytics', // default
+          anonymize: true, // default
+          allowAdFeatures: false // default
+        },
+        googleTagManager: {
+          trackingId: 'YOUR_GOOGLE_TAG_MANAGER_TRACKING_ID', // leave empty if you want to disable the tracker
+          cookieName: 'gatsby-gdpr-google-tagmanager', // default
+          dataLayerName: 'dataLayer', // default
+        },
+        facebookPixel: {
+          pixelId: 'YOUR_FACEBOOK_PIXEL_ID', // leave empty if you want to disable the tracker
+          cookieName: 'gatsby-gdpr-facebook-pixel', // default
+        },
+        // defines the environments where the tracking should be available  - default is ["production"]
+        environments: ['production', 'development']
+      },
+    },
+
     {
       resolve: `gatsby-source-contentful`,
       options: {
