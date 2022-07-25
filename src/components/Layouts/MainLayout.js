47b43a9e347ef215fbf82f55170bd2edb9814ff2
Chelsea Alcinord <59550990+seasea19@users.noreply.github.com>
2022-07-25 11:57:20 -0400

    changes made

--- src/components/Layouts/MainLayout.js
+++ src/components/Layouts/MainLayout.js
@@ -8,7 +8,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { StaticQuery, graphql } from "gatsby";
-import CookieConsent from "react-cookie-consent";
+import Cookiebanner from "../Cookiebanner";
 import Header from "../Header";
 import Footer from "../Footer";
 
@@ -27,45 +27,11 @@ const Layout = ({ children }) => {
           }
         }
       `}
+      
       render={(data) => (
         <>
           <Header siteTitle={data.site.siteMetadata.title} />
-          <CookieConsent
-            style={{ background: "black", textShadow: "2rem 2rem black" }}
-            enableDeclineButton
-            flipButtons
-            buttonStyle={{
-              background: "#ffffff",
-              color: "black",
-            }}
-            declineButtonStyle={{
-              background: "#B0ADAC",
-              color: "black",
-            }}
-            location="bottom"
-            buttonText="I accept"
-            declineButtonText="Decline"
-            cookieSecurity="true"
-            sameSite="none"
-            cookieName="gatsby-gdpr-google-analytics"
-          >
-            This site uses cookies and related technologies, as described in our{" "}
-            <a
-              aria-label="learn more about cookies"
-              role="button"
-              tabindex="0"
-              class="cc-link"
-              href="https://www.vngle.com/privacy"
-              rel="noopener noreferrer nofollow"
-              target="_blank"
-            >
-              privacy
-            </a>{" "}
-            , for purposes that may include site operation, analytics, enhanced
-            user experience, or advertising. You consent to our cookies if you
-            continue to use our website. However, if you do not accept cookies,
-            you may not be able to use some portions of our services.
-          </CookieConsent>
+          <Cookiebanner />
           <main>{children}</main>
           <Footer />
         </>
