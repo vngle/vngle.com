5f48ec1b03a164d174b9d3067d01729d7c98a0aa
Kenneth Chen <efeichen@gmail.com>
2022-08-08 12:03:01 -0400

    Update dependencies

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,24 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "vngleweb16f5322c": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "HostedUIDomain": "string",
+            "OAuthMetadata": "string",
+            "UserPoolId": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "AppClientSecret": "string"
+        },
+        "userPoolGroups": {
+            "AdminGroupRole": "string"
+        }
+    },
+    "api": {
+        "storycms": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
